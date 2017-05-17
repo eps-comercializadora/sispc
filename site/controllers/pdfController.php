b56a18214f6ed40425b37ba8884b8b7e5a3603ed
angel charlot <legna18205@gmail.com>
2017-05-17 17:04:16 -0400

    ultimos cambios, falta el peso para el ultimo reporte

--- site/controllers/pdfController.php
+++ site/controllers/pdfController.php
@@ -55,12 +55,8 @@ class pdfController extends Controller
 
 
 
-			$this->_pdf->Output();
-	
-
-	}
-	
-	    public function cruze2(){
+			$this->_pdf->Output();}
+	public function cruze2(){
 
 	
 
@@ -105,8 +101,56 @@ class pdfController extends Controller
 
 
 				}
-				$this->_pdf->Output();
-}
+				$this->_pdf->Output();}
+	public function reporte1(){
+
+	
+
+			$array=$this->cruze->reporte1();
+
+
+			/*print_r($res);
+
+
+			$this->_pdf->AddPage();
+			$this->_pdf->SetFont('Arial','B',12);
+
+
+			$this->_pdf->SetFont('Arial','B',12);
+			$this->_pdf->Cell(190,8, utf8_decode('LISTA'),0,1,'C');
+			$this->_pdf->Ln(15);
+			$this->_pdf->SetFont('Arial','',8);
+
+			
+		
+			$this->_pdf->SetFont('Arial','',6);
+			for ($i=1; $i <= count($array)  ; $i++) { 
+			$this->_pdf->Cell(5,6, utf8_decode('Nº'),1,0,'C');
+			$this->_pdf->Cell(14,6, utf8_decode('COD.SICA'),1,0,'C');
+			$this->_pdf->Cell(14,6, utf8_decode('RIF'),1,0,'C');
+			$this->_pdf->Cell(100,6, utf8_decode('DESCRIPCION'),1,0,'C');	
+			$this->_pdf->Cell(58,6, utf8_decode('COD.FACTURA'),1,1,'C');
+			
+			$this->_pdf->Cell(5,6, $i,1,0,'C');
+			$this->_pdf->Cell(14,6, utf8_decode($array[$i]['5']),1,0,'C');
+			$this->_pdf->Cell(14,6, $array[$i]['0'],1,0,'L');
+			$this->_pdf->Cell(100,6, utf8_decode($array[$i]['1']),1,0,'L');	
+			$this->_pdf->Cell(58,6, utf8_decode($array[$i]['2']),1,1,'C');
+			$this->_pdf->Cell(191,6, utf8_decode("Fecha de la ultima factura:".$array[$i]['4']->format('d/m/Y')),1,1,'C');
+
+				for ($f=0; $f < count($array[$i][6]) ; $f++) { 
+					
+			$this->_pdf->Cell(58,6, utf8_decode($array[$i][6][$f][1]),0,0,'L');	
+			$this->_pdf->Cell(58,6, utf8_decode($array[$i][6][$f][0]),0,0,'L');
+			$this->_pdf->Cell(58,6, intval($array[$i][6][$f][2]),0,1,'L');
+				}
+
+
+				}
+				$this->_pdf->Output();*/
+
+			}
+
 
 
 
--- site/models/cruzeModel.php
+++ site/models/cruzeModel.php
@@ -14,129 +14,191 @@ class cruzeModel extends Model
 public function cruze(){
 
 
-$sql="select * from empresa_cargada order by rif";
+        $sql="select * from empresa order by rif";
 
-$datos = $this->_db->query($sql);
-        
-$rs_mysql=$datos->fetchall();
-
-
+        $datos = $this->_db->query($sql);
+                
+        $rs_mysql=$datos->fetchall();
 
 
-$serverName = "servidor";
-$connectionInfo = array( "Database"=>"enterpriseadmindb", "UID"=>"sa", "PWD"=>"9822");
-$conn = sqlsrv_connect( $serverName, $connectionInfo );
-if( $conn === false ) {
-    die( print_r( sqlsrv_errors(), true));
-}
 
 
-$x=1;
-
-for ($i=0; $i < count($rs_mysql) ; $i++) { 
-	
-
-$sql = "SELECT id3, descrip, Represent,Telef FROM saclie where id3='".$rs_mysql[$i]['tipo_rif_id'].$rs_mysql[$i]['rif']."' order by id3";
-$stmt = sqlsrv_query( $conn, $sql );
-if( $stmt === false) {
-    die( print_r( sqlsrv_errors(), true) );
-}
-
-while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC) ) {
-//echo $x;
-//print_r($row);
+        $serverName = "servidor";
+        $connectionInfo = array( "Database"=>"enterpriseadmindb", "UID"=>"sa", "PWD"=>"9822");
+        $conn = sqlsrv_connect( $serverName, $connectionInfo );
+        if( $conn === false ) {
+            die( print_r( sqlsrv_errors(), true));
+        }
 
 
-$array[$x]=$row;
-$array[$x][]=$rs_mysql[$i]['codigo_sica'];
+        $x=1;
+
+        for ($i=0; $i < count($rs_mysql) ; $i++) { 
+        	
+
+        $sql = "SELECT id3, descrip, Represent,Telef FROM saclie where id3='".$rs_mysql[$i]['tipo_rif_id'].$rs_mysql[$i]['rif']."' order by id3";
+        $stmt = sqlsrv_query( $conn, $sql );
+        if( $stmt === false) {
+            die( print_r( sqlsrv_errors(), true) );
+        }
+
+        while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC) ) {
+        //echo $x;
+        //print_r($row);
 
 
-$x++;
+        $array[$x]=$row;
+        $array[$x][]=$rs_mysql[$i]['codigo_sica'];
 
-}
 
+        $x++;
 
+        }
 
 
 
 
 
 
-}
 
 
-return $array;
-}
+        }
 
+
+        return $array;}
 public function cruze2(){
 
 
-$sql="select * from empresa_cargada order by rif";
+      $sql="select * from empresa order by rif";
 
-$datos = $this->_db->query($sql);
-        
-$rs_mysql=$datos->fetchall();
-
-
+      $datos = $this->_db->query($sql);
+              
+      $rs_mysql=$datos->fetchall();
 
 
-$serverName = "servidor";
-$connectionInfo = array( "Database"=>"enterpriseadmindb", "UID"=>"sa", "PWD"=>"9822");
-$conn = sqlsrv_connect( $serverName, $connectionInfo );
-if( $conn === false ) {
-    die( print_r( sqlsrv_errors(), true));
-}
 
 
-$x=1;
+      $serverName = "servidor";
+      $connectionInfo = array( "Database"=>"enterpriseadmindb", "UID"=>"sa", "PWD"=>"9822");
+      $conn = sqlsrv_connect( $serverName, $connectionInfo );
+      if( $conn === false ) {
+          die( print_r( sqlsrv_errors(), true));
+      }
 
-for ($i=0; $i < count($rs_mysql) ; $i++) { 
-	
 
-$sql = "SELECT SACLIE.CodClie,SACLIE.Descrip,SAFACT.NumeroD,SAFACT.tipofac,SAFACT.fechai
-FROM SACLIE SACLIE INNER JOIN SAFACT SAFACT ON 
-     (SAFACT.NumeroD = SACLIE.NumeroUV)
-      AND (SAFACT.CodClie = SACLIE.CodClie)
-      and (SACLIE.CodClie = '".$rs_mysql[$i]['tipo_rif_id'].$rs_mysql[$i]['rif']."')";
+      $x=1;
+
+      for ($i=0; $i < count($rs_mysql) ; $i++) { 
+      	
+
+      $sql = "SELECT SACLIE.CodClie,SACLIE.Descrip,SAFACT.NumeroD,SAFACT.tipofac,SAFACT.fechai
+      FROM SACLIE SACLIE INNER JOIN SAFACT SAFACT ON 
+           (SAFACT.NumeroD = SACLIE.NumeroUV)
+            AND (SAFACT.CodClie = SACLIE.CodClie)
+            and (SACLIE.CodClie = '".$rs_mysql[$i]['tipo_rif_id'].$rs_mysql[$i]['rif']."')";
+
+      $stmt = sqlsrv_query( $conn, $sql );
+      while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC) ) {
+      //echo $x;
+      //print_r($row);
+
+
+      $array[$x]=$row;
+      $array[$x][]=$rs_mysql[$i]['codigo_sica'];
+
+
+
+
+
+      $sql = "SELECT SAITEMFAC.Descrip1,SAITEMFAC.Coditem,SAITEMFAC.Cantidad FROM SAITEMFAC where NumeroD='".$array[1][2]."'
+       ";
+      $stmt = sqlsrv_query( $conn, $sql );
+      if( $stmt === false) {
+          die( print_r( sqlsrv_errors(), true) );
+      }
+      while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC) ) {
+
+      $array[$x][6][]=$row;
+
+      }
+
+
+
+
+      $x++;
+
+      }
+
+      }
+
+
+
+      return $array;
+    }
+public function reporte1(){
+
+
+    
+
+      //conexion
+      $serverName = "servidor";
+      $connectionInfo = array( "Database"=>"enterpriseadmindb", "UID"=>"sa", "PWD"=>"9822");
+      $conn = sqlsrv_connect( $serverName, $connectionInfo );
+     
+      //consulta
+
+    $sql = "SELECT 
+    SAFACT.fechai as fecha_defactura,
+    SAFACT.NumeroD as nro_factura,
+    SAFACT.descrip as nombre_cliente,
+    SAFACT.contado as monto_contado,
+    SAFACT.credito as monto_credito,
+    SAFACT.notas1 as autoriza,
+    SAVEND.descrip as vendedor,
+    SACLIE.movil as tlf_cliente,
+    SAFACT.notas2 as municipio_parroquia,
+    SAOPER.descrip as operacion,
+    SAFACT.notas3 as combos_cajas
+FROM SAFACT SAFACT INNER JOIN SAOPER SAOPER ON 
+     (SAOPER.CodOper = SAFACT.CodOper)
+      INNER JOIN SACLIE SACLIE ON 
+     (SACLIE.CodClie = SAFACT.CodClie) and (SACLIE.CodClie = 'V 20574205')
+      INNER JOIN SAVEND SAVEND ON 
+     (SAVEND.CodVend = SAFACT.CodVend)
+     ";
+
+    //send a db
+
+    $stmt = sqlsrv_query( $conn, $sql );
+    while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
+
+    $array[]=$row;
+    
+    }
+
+    print_r($array);
+
+/*
+for ($i=0; $i < count($array) ; $i++) { 
+
+
 
 $stmt = sqlsrv_query( $conn, $sql );
-while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC) ) {
-//echo $x;
-//print_r($row);
+    while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
 
+    $array[$i]=$row;
+    
+    }
 
-$array[$x]=$row;
-$array[$x][]=$rs_mysql[$i]['codigo_sica'];
-
-
-
-
-
-$sql = "SELECT SAITEMFAC.Descrip1,SAITEMFAC.Coditem,SAITEMFAC.Cantidad FROM SAITEMFAC where NumeroD='".$array[1][2]."'
- ";
-$stmt = sqlsrv_query( $conn, $sql );
-if( $stmt === false) {
-    die( print_r( sqlsrv_errors(), true) );
-}
-while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_NUMERIC) ) {
-
-$array[$x][6][]=$row;
-
+ 
 }
 
+*/
 
 
 
-$x++;
 
-}
-
-}
-
-
-
-return $array;
-}
+    }
 
 
 
